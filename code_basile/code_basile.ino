f32deb0f95bfb56d6d8de571ec2da12cbbb31c15
Bears9355 <52669057+Bears9355@users.noreply.github.com>
2023-02-22 12:00:03 +0100

    Ajout du calcul de distance
    ⚠️ La valeur obtenue est fausse, en attente de calibrage

--- code_basile/code_basile.ino
+++ code_basile/code_basile.ino
@@ -7,6 +7,8 @@
 
   Les capteurs/modules utilisent le bus SPI ou analogique.
 
+  La valeur de distance du capteur IR est fausse, c'est en attendant un calibrage précis.
+
   /!\ IMPORTANT : /!\
   /!\ Veuillez mettre un fichier nommé "donnees.csv" dans la carte SD.  /!\
   /!\ Ce fichier doit être VIDE. Importez le code avant d'insérer la    /!\
@@ -40,16 +42,19 @@ Adafruit_BME680 bme(BME_CS); // hardware SPI
 //Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
 int time;
+int irPin = 0; // Pin sur lequel est branché la sortie analogique du capteur IR
 
 void setup() {
   Serial.begin(9600);
   SD.begin(SD_CS);
   while (!Serial);
+  Serial.println();
+  Serial.println();
   Serial.println(F("BME680 test"));
   time = 0;
 
   if (!bme.begin()) {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    Serial.println("Impossible de trouver un BME680 valide, verifiez les cablages!");
     while (1);
   }
 
@@ -79,19 +84,24 @@ void setup() {
 
 void loop() {
   if (! bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
+    Serial.println("Impossible de lire sur le BME680 :(");
     return;
   }
 
-  // Lecture des données
+  // Lecture des données du BME680
   float tauxHumidite = bme.humidity;        // Lecture du taux d'humidité, exprimé en %
   float temperature = bme.temperature;      // Lecture de la température ambiante, exprimée en degrés celsius
-  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  float pression = (bme.pressure / 100.0);
-  float resistanceGaz = (bme.gas_resistance / 1000.0);
+  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA); // Lecture de l'altitude grâce à la pression en m
+  float pression = (bme.pressure / 100.0); // Lecture de la pression atmosphérique en hPa
+  float resistanceGaz = (bme.gas_resistance / 1000.0); // Lecture de la résistance électrique du capteur de gaz en kOhm
+
+  // Lecture des données du capteur IR (Gravimètre)
+  int sensorValue = analogRead(irPin); // Lit la valeur analogique du capteur
+  float distance = (6787.0 / (sensorValue - 3.0)) - 4.0; // Calcule la distance en cm
+  // /!\ CETTE VALEUR DE DISTANCE EST FAUSSE /!\
 
   // Vérification si données bien reçues
-  if (isnan(tauxHumidite) || isnan(temperature) || isnan(altitude) || isnan(pression) || isnan(resistanceGaz)) {
+  if (isnan(tauxHumidite) || isnan(temperature) || isnan(altitude) || isnan(pression) || isnan(resistanceGaz) || isnan(distance)) {
     Serial.println(F("Une des valeurs du BME n'est pas valide ou absente. Est-il bien branche ?"));
     delay(2000);
     return;         // Si aucune valeur n'a été reçue par l'Arduino, on attend 2 secondes, puis on redémarre la fonction loop()
@@ -103,19 +113,22 @@ void loop() {
   String altitudeArrondie = String(altitude,1);           // Affichage d'une seule décimale (car précision de 0.1 m ici)
   String pressionArrondie = String(pression,1);           // Affichage d'une seule décimale (car précision de 0.6 hPa ici)
   String gazArrondie = String(resistanceGaz,1);           // Affichage d'une seule décimale 
+  String distanceArrondie = String(distance, 1);          // Affichage d'une seule décimale
   tauxHumiditeArrondi.replace(".", ",");                  // et on remplace les séparateurs de décimale, initialement en "." pour devenir ","
   temperatureArrondie.replace(".", ",");                  // (permet par exemple d'affichier 12,76 au lieu de 12.76 (ce qui facilite l'interprétation dans Excel ou tout autre tableur)
   altitudeArrondie.replace(".", ",");
   pressionArrondie.replace(".", ",");
   gazArrondie.replace(".", ",");
+  distanceArrondie.replace(".", ",");
 
   // Affichage des valeurs sur le moniteur série de l'IDE Arduino
   Serial.print("Humidity = "); Serial.print(tauxHumiditeArrondi); Serial.print(" % - ");
   Serial.print("Temperature = "); Serial.print(temperatureArrondie); Serial.println(" *C - ");
-  Serial.print("Altitude = "); Serial.print(altitudeArrondie); Serial.println(" m - ");
+  Serial.print("Altitude = "); Serial.print(altitudeArrondie); Serial.print(" m - ");
   Serial.print("Pression = "); Serial.print(pressionArrondie); Serial.println(" hPa - ");
-  Serial.print("Gaz = "); Serial.print(gazArrondie); Serial.println(" kOhm - ");
-  Serial.print("Temps : "); Serial.print(time); Serial.println("");
+  Serial.print("Gaz = "); Serial.print(gazArrondie); Serial.print(" kOhm - ");
+  Serial.print("Temps : "); Serial.print(time); Serial.println(" secondes - ");
+  Serial.print("Distance = "); Serial.print(distance); Serial.print(" cm - ");
 
   // Enregistrement de ces données sur la carte SD
   monFichier = SD.open(nomDuFichier, FILE_WRITE);
@@ -131,6 +144,8 @@ void loop() {
     monFichier.print(gazArrondie);
     monFichier.print(";");
     monFichier.print(time);
+    monFichier.print(";");
+    monFichier.print(distanceArrondie);
     monFichier.println();
     monFichier.close();                     // L'enregistrement des données se fait au moment de la clôture du fichier
     Serial.println(F("Enregistrement reussi en carte SD"));
